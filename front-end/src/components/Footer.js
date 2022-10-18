4bfad594e96c12a968bac6a6a50d79686e8193fc
Rashed Rifat <rmr557@nyu.edu>
2022-10-18 14:09:25 -0400

    adding sample footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,8 +1,16 @@
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import {
+  Box
+} from "@mui/material"; 
 
-const Footer = (props) => {
-  return <p>Placeholder Footer Component!</p>;
+const Footer = props => {
+  return <Box>
+    <footer className="strangerFooter">
+      <Link to="/" className="footerHomeLink">Home</Link> 
+      &copy;HelloStranger, 20222
+    </footer>
+  </Box>
 };
 
 export default Footer;
