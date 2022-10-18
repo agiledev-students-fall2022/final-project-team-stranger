7b206a2ad11605b0e9ac7d7b4480ae254ec72b08
L-Gu7 <tianhao.gu@outlook.com>
2022-10-18 18:06:13 -0400

    Add test behavior

--- front-end/src/components/Message.js
+++ front-end/src/components/Message.js
@@ -1,10 +1,26 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./Message.css";
+import { useNavigate } from "react-router-dom";
 import { Paper, TextField, Button } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import SendIcon from "@mui/icons-material/Send";
 
 const Message = (props) => {
+  const [message, setMessage] = useState("");
+  //const [err, setErr] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //test behavior
+    if (message === "") {
+      alert("Message cannot be empty.");
+    } else {
+      alert(`Your message:\n${message}\nis successfully sent!`);
+      navigate("/"); //redirect to home
+    }
+  };
+
   return (
     <>
       <Grid2 container sx={{ m: "0px 15px" }}>
@@ -12,22 +28,29 @@ const Message = (props) => {
           <h3>New Message</h3>
         </Grid2>
         <Grid2 item xs={12}>
-          <Paper variant="">
+          <form onSubmit={handleSubmit}>
             <TextField
-              id="newMessage"
+              value={message}
               margin="normal"
               variant="outlined"
               label="Share your warmth with strangers"
               fullWidth
               multiline
               rows={12}
+              onChange={(e) => setMessage(e.target.value)}
+              helperText={message === "" ? "Message cannot be empty" : " "}
             />
             <Grid2 item xs={4} xsOffset={8} smOffset={9}>
-              <Button variant="contained" endIcon={<SendIcon />} size="large">
+              <Button
+                type="submit"
+                variant="contained"
+                endIcon={<SendIcon />}
+                size="large"
+              >
                 Send
               </Button>
             </Grid2>
-          </Paper>
+          </form>
         </Grid2>
       </Grid2>
     </>
