399197286dcfb8815c4ac5ee2c41de270bc499a4
Richard <feavfas@gmail.com>
2022-10-19 00:55:19 -0400

    added sidebar component

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -1,8 +1,19 @@
 import "./Profile.css";
+import Sidebar from "./components/Sidebar";
 import { Link } from "react-router-dom";
-
+const userDetails = { username: "bob", userProfile: "temp" };
 const Profile = (props) => {
-  return <p>Placeholder Profile Page!</p>;
+  return (
+    <div id="Profile">
+      <Sidebar
+        pageWrapId={"page-wrap"}
+        outerContainerId={"Profile"}
+        userDetails={userDetails}
+      />
+
+      <div id="page-wrap"></div>
+    </div>
+  );
 };
 
 export default Profile;
--- front-end/src/components/Sidebar.js
+++ front-end/src/components/Sidebar.js
@@ -1,9 +1,27 @@
-import "./Sidebar.css"
+import "./Sidebar.css";
+import { slide as Menu } from "react-burger-menu";
 
-const Sidebar = props => {
+const Sidebar = (props) => {
+  const { pageWrapId, outerContainerId, userDetails } = props;
   return (
-    <p>Placeholder Sidebar!</p>
-  )
-}
+    <>
+      <Menu pageWrapId={pageWrapId} outerContainerId={outerContainerId}>
+        <div>
+          {Object.entries(userDetails).map(([key, value]) => (
+            <>
+              <div key={value}>{value}</div>
+            </>
+          ))}
+        </div>
+        <a id="sign-in" className="menu-item" href="/sign-in">
+          Sign out
+        </a>
+        <a id="settings" className="menu-item" href="/settings">
+          Settings
+        </a>
+      </Menu>
+    </>
+  );
+};
 
-export default Sidebar; 
+export default Sidebar;
