b9232a9b5c5e01adfaa114cc0ccbac03031e2f98
Rashed Rifat <rmr557@nyu.edu>
2022-10-20 13:01:58 -0400

    updating footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,8 +1,21 @@
 import "./Footer.css";
-import { Link } from "react-router-dom";
+import {
+  BottomNavigation, BottomNavigationAction, Paper
+} from "@mui/material"; 
+import HomeIcon from "@mui/icons-material/Home";
+import SendIcon from "@mui/icons-material/Send";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
 
-const Footer = (props) => {
-  return <p>Placeholder Footer Component!</p>;
-};
+const Footer = props => {
+  return <Paper sx={{
+    position: "fixed", bottom: 0, left: 0, right: 0 
+  }} elevation={3}>
+    <BottomNavigation showLabels>
+      <BottomNavigationAction label="Home" icon={<HomeIcon/>} href="/"/>
+      <BottomNavigationAction label="Send Message" icon={<SendIcon/>} href="/send-message"/>
+      <BottomNavigationAction label="Stats" icon={<QueryStatsIcon/>} href="/stats"/>
+    </BottomNavigation>
+  </Paper>
+}
 
 export default Footer;
