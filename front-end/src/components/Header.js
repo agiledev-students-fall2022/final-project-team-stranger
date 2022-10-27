7852aa3187625d033cc20274bcedd4b126060299
L-Gu7 <tianhao.gu@outlook.com>
2022-10-27 02:58:36 -0400

    Reimplement sidebar with mui drawer

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,34 +1,35 @@
 import "./Header.css";
-import {
-  AppBar, Toolbar, Button, Box, IconButton
-} from "@mui/material"
+import { AppBar, Toolbar, Button, Box, IconButton } from "@mui/material";
 import LogoIcon from "./LogoIcon";
-
+import Sidebar from "./Sidebar";
 // Reference Link: https://mui.com/material-ui/react-app-bar/#main-content
 
 const pages = {
-  "Home" : "/", 
-  "Send Message" : "/send-message", 
-  "Stats" : "/stats"
-}; 
+  Home: "/",
+  "Send Message": "/send-message",
+  Stats: "/stats",
+};
 
 const Header = (props) => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
+        <Sidebar />
         <IconButton href="/" fontSize="large">
-          <LogoIcon color="primary" sx={{fontSize : "3rem"}}/>
+          <LogoIcon color="primary" sx={{ fontSize: "3rem" }} />
         </IconButton>
         <Box>
-          {Object.keys(pages).map(page => {
-            return <Button key={page} color="inherit" href={pages[page]}>
-              {page}
-            </Button>
+          {Object.keys(pages).map((page) => {
+            return (
+              <Button key={page} color="inherit" href={pages[page]}>
+                {page}
+              </Button>
+            );
           })}
         </Box>
       </Toolbar>
     </AppBar>
-  )
+  );
 };
 
 export default Header;
--- front-end/src/components/Sidebar.js
+++ front-end/src/components/Sidebar.js
@@ -1,9 +1,64 @@
-import "./Sidebar.css"
+import React, { useState } from "react";
+import "./Sidebar.css";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-const Sidebar = props => {
+const Sidebar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
   return (
-    <p>Placeholder Sidebar!</p>
-  )
-}
+    <>
+      <IconButton>
+        <AccountCircleIcon onClick={toggleDrawer} />
+      </IconButton>
+      <SwipeableDrawer id="menu" open={isOpen} onClose={toggleDrawer}>
+        <Box id="profile">
+          <Avatar>U</Avatar>
+          <br />
+          <div>USERNAME</div>
+          <div>JOINED 99 DAYS AGO</div>
+        </Box>
 
-export default Sidebar; 
+        <List className="actions">
+          <ListItem disablePadding>
+            <ListItemButton id="sign-in" className="menu-item" href="/sign-in">
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sign out" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton
+              id="settings"
+              className="menu-item"
+              href="/settings"
+            >
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+    </>
+  );
+};
+
+export default Sidebar;
