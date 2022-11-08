e370d4813a3bd9b4c71d4a0aace43db754306da5
Youyu Zhu <youyuzhu@10-16-230-36.dynapool.wireless.nyu.edu>
2022-11-08 18:08:19 -0500

    Unit test

--- back-end/tests/test.js
+++ back-end/tests/test.js
@@ -1,11 +1,8 @@
-
-const downsort=require("/Users/youyuzhu/Desktop/agile/final-project-team-stranger/back-end/logic/HistoryFunction.js")
 const chai = require('chai');
 const expect=require("chai")
 const app=require("/Users/youyuzhu/Desktop/agile/final-project-team-stranger/back-end/app.js")
 const chaiHttp = require('chai-http');
 const should = chai.should();
-const downsort=require("/Users/youyuzhu/Desktop/agile/final-project-team-stranger/back-end/logic/HistoryFunction.js")
 chai.use(chaiHttp);
 
 describe("Demo test for the History", () => {
@@ -17,47 +14,5 @@ describe("Demo test for the History", () => {
             res.should.have.status(200);
             done()
         })
-    })
-})
-
-
-describe("Downsort basic functionality", ()=>{
-    it("returns an object with downsort order in given string:",()=>{
-        const sample=[            
-            {
-                "index":2,
-                "name":"bb",
-                "score":50
-            },
-            {
-                "index":1,
-                "name":"cc",
-                "score":100
-            },
-            {
-                "index":3,
-                "name":"aa",
-                "score":200
-            }
-        ]
-        const expected=[
-            {
-                "index":1,
-                "name":"cc",
-                "score":100
-            },
-            {
-                "index":2,
-                "name":"bb",
-                "score":50
-            },
-            {
-                "index":3,
-                "name":"aa",
-                "score":200
-            }
-        ]
-        const actual=sample.sort(downsort("name"))
-        ac;
     })
 })
