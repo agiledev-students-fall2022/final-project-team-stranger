2d0fbbd49db410fffbf3de1026b82a4635696bf9
Youyu Zhu <youyuzhu@10-17-173-205.dynapool.wireless.nyu.edu>
2022-11-09 18:50:33 -0500

    Unit Tests added

--- back-end/tests/test.js
+++ back-end/tests/test.js
@@ -6,12 +6,65 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Demo test for the History", () => {
-    it("Should respond with a json object!", done => {
+    it("Should respond with 200!", done => {
         chai
         .request(app)
         .get("/history")
         .end((err, res) => {
             res.should.have.status(200);
+
+            done()
+        })
+    })
+    it("Should respond with a json object!", done => {
+        chai
+        .request(app)
+        .get("/history")
+        .end((err, res) => {
+            res.should.to.be.json;
+
+            done()
+        })
+    })
+    it("Should respond with an array!", done => {
+        chai
+        .request(app)
+        .get("/history")
+        .end((err, res) => {
+            res.body.should.be.a('array');
+
+            done()
+        })
+    })
+})
+describe("Demo test for the Stats", () => {
+    it("Should respond with 200!", done => {
+        chai
+        .request(app)
+        .get("/stats")
+        .end((err, res) => {
+            res.should.have.status(200);
+
+            done()
+        })
+    })
+    it("Should respond with a json object!", done => {
+        chai
+        .request(app)
+        .get("/stats")
+        .end((err, res) => {
+            res.should.to.be.json;
+
+            done()
+        })
+    })
+    it("Should respond with an array!", done => {
+        chai
+        .request(app)
+        .get("/stats")
+        .end((err, res) => {
+            res.body.should.be.a('array');
+
             done()
         })
     })
