8c067c1c6ca8e19b1a57e56fcd350297dc6e0364
Youyu Zhu <youyuzhu@Youyus-MacBook-Air.local>
2022-11-24 17:02:57 -0500

    SideBar

--- back-end/app.js
+++ back-end/app.js
@@ -31,7 +31,6 @@ const StatsRouter = require('./logic/StatsFunction.js');
 const settingsRouter = require("./logic/settingsRouter.js");
 const messageRouter = require("./logic/MessageRouter.js");
 const SidebarRouter = require("./logic/sidebarRouter.js");
-const LogoutRouter = require("./logic/logoutRouter.js");
 
 // Middleware 
 app.use(morgan("dev", { skip: (req, res) => process.env.NODE_ENV === "test" })); // log all incoming requests
@@ -61,8 +60,6 @@ app.use("/", passport.authenticate("strangerLogin", {
 app.use("/", passport.authenticate("strangerLogin", {
   session: false, failureRedirect: '/authFail'}), SidebarRouter);
 
-app.use("/", passport.authenticate("strangerLogin", {
-  session: false, failureRedirect: '/authFail'}), LogoutRouter);
 
 // Sample Endpoint 
 app.get("/", (req, res) => {
